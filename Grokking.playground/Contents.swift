dda8c8a346a9f0ac7a5995bf82e5c1e3c69700fa
Pedro Alonso <pedro.alonsod@gmail.com>
2017-06-14 10:36:37 -0700

    graphs
    need more unertanding of how to make them

--- Grokking.playground/Contents.swift
+++ Grokking.playground/Contents.swift
@@ -1,5 +1,17 @@
 import UIKit
 
+class Queue {
+    var queue: [[String]] = []
+    
+    func enqueue(name: [String]) {
+        self.queue.append(name)
+    }
+    
+    func dequeu() -> [String] {
+        return self.queue.removeFirst()
+    }
+}
+
 func binanrySearch(a: [Int], item: Int) -> Int? {
     var low = 0
     var high = a.count - 1
@@ -42,4 +54,88 @@ func selectionSort(a: inout [Int]) -> [Int] {
     return newA
 }
 
-selectionSort(a: &a)
+var nA = selectionSort(a: &a)
+
+func sum(a: inout [Int]) -> Int {
+    
+    if a.count == 0 {
+        return 0
+    }
+    
+    if a.count == 1 {
+        return a[0]
+    }
+    
+    return a.removeFirst() + sum(a: &a)
+}
+
+sum(a: &nA)
+
+func countRecursive(a: inout [Int]) -> Int {
+    if a.count == 1 {
+        return a.count
+    }
+    a.removeFirst()
+    return 1 + countRecursive(a: &a)
+}
+
+var nB = [1, 2, 3, 4, 5]
+
+countRecursive(a: &nB)
+
+func quickSort(a: inout [Int]) -> [Int] {
+    if a.count < 2 {
+        return a
+    } else {
+        let pivot = a[0]
+        var lesser = a.filter { $0 < pivot }
+        var greater = a.filter { $0 > pivot }
+        
+        return quickSort(a: &lesser) + Array([pivot]) + quickSort(a: &greater)
+    }
+}
+
+var qS = [4, 6, 8, 10, 66, 40, 3, 1, 2]
+
+quickSort(a: &qS)
+
+//hash tables
+var hashT: [String:Int] = ["1":2,"2":1]
+var graph: [String: [String]] = [:]
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+//BFS queue
+var q: Queue = Queue()
+q.enqueue(name: graph["you"]!)
+
+//while !q.queue.isEmpty {
+//    var person = q.dequeu()
+//    if person.contains("peggy") {
+//        print(person)
+//        break
+//    } else {
+//        q.enqueue(name: [person.removeFirst()])
+//    }
+//}
+
+//def search(name):
+//search_queue = deque() search_queue += graph[name] searched = []
+//while search_queue:
+//person = search_queue.popleft() if not person in searched:
+//    if person_is_seller(person):
+//print person + “ is a mango seller!” return True
+//This array is how you keep track of which people you’ve searched before.
+//Only search this person if you
+//Marks this person as searched
+//else:
+//search_queue += graph[person] searched.append(person)
+//return False
+
+
