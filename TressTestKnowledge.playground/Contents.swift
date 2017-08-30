d3ebd3dfbee097bfc0040320304a8b3184ff6c12
Pedro Alonso <ealonso@grupoeco.com.mx>
2017-08-30 13:17:11 -0700

    Trees 2nd attempt

--- TressTestKnowledge.playground/Contents.swift
+++ TressTestKnowledge.playground/Contents.swift
@@ -71,3 +71,112 @@ testTree.insert(data: 5)
 print(testTree.printTree(node: testTree.root))
 testTree.count
 
+//Second attempt done in notebook
+class NodeBST<T: Comparable> {
+    var data: T
+    var left: NodeBST<T>?
+    var right: NodeBST<T>?
+    
+    init(data: T) {
+        self.data = data
+    }
+}
+
+class BSTTree<T: Comparable> {
+    var root: NodeBST<T>?
+    
+    func preOrder() {
+        guard root != nil else { return }
+        preOrderHelper(node: root)
+    }
+    func preOrderHelper(node: NodeBST<T>?) {
+        if let node = node {
+            print("\(node.data)")
+            preOrderHelper(node: node.left)
+            preOrderHelper(node: node.right)
+        }
+    }
+    
+    func inOrder() {
+        guard root != nil else { return }
+        inOrderHelper(node: root)
+    }
+    func inOrderHelper(node: NodeBST<T>?) {
+        if let node = node {
+            inOrderHelper(node: node.left)
+            print(node.data)
+            inOrderHelper(node: node.right)
+        }
+    }
+    
+    func postOrder() {
+        guard root != nil else { return }
+        postOrderHelper(node: root)
+    }
+    func postOrderHelper(node: NodeBST<T>?) {
+        if let node = node {
+            postOrderHelper(node: node.left)
+            postOrderHelper(node: node.right)
+            print("\(node.data)")
+        }
+    }
+    
+    func levelOrder() {
+        guard root != nil else { return }
+        levelOrderHelper(node: root)
+    }
+    func levelOrderHelper(node: NodeBST<T>?) {
+        var q: [NodeBST<T>] = []
+        var tmp: NodeBST<T>?
+        guard root != nil else { return }
+        q.append(root!)
+        while !q.isEmpty {
+            tmp = q.removeFirst()
+            print("\(tmp!.data)")
+            if tmp?.left != nil {
+                q.append(tmp!.left!)
+            }
+            if tmp?.right != nil {
+                q.append(tmp!.right!)
+            }
+        }
+    }
+    
+    func insert(data: T) {
+        if root == nil {
+            root = NodeBST<T>(data: data)
+            root?.left = nil
+            root?.right = nil
+        } else {
+            insertHelper(data: data, node: root)
+        }
+    }
+    func insertHelper(data: T, node: NodeBST<T>?) {
+        if data > node!.data {
+            if let right = node?.right {
+                insertHelper(data: data, node: right)
+            } else {
+                node!.right = NodeBST<T>(data: data)
+                node!.right?.right = nil
+                node!.left?.left = nil
+            }
+        } else {
+            if let left = node?.left {
+                insertHelper(data: data, node: left)
+            } else {
+                node!.left = NodeBST<T>(data: data)
+                node!.left?.right = nil
+                node!.left?.left = nil
+            }
+        }
+    }
+}
+
+var bstTest = BSTTree<Int>()
+
+bstTest.insert(data: 10)
+bstTest.insert(data: 20)
+bstTest.insert(data: 5)
+bstTest.inOrder()
+print("*****")
+bstTest.levelOrder()
