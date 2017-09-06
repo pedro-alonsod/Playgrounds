edd1f3637240514fd39c30b4fb9bccd503756791
Pedro Alonso <ealonso@grupoeco.com.mx>
2017-09-05 20:43:58 -0700

    grokking recursion

--- GrokkingAlgorithms.playground/Contents.swift
+++ GrokkingAlgorithms.playground/Contents.swift
@@ -43,10 +43,42 @@ func findSmallest(arr: [Int]) -> Int {
 func selectionSort(arr: [Int]) -> [Int] { // Sorts an array
     var newArr: [Int] = []
     var copy = arr
-    for  i in 0..<arr.count {
+    for  i in 0..<copy.count {
         var smallest = findSmallest(arr: copy) // Finds the smallest element in the array,
         newArr.append(copy.remove(at: smallest)) //  and adds it to the new array
     }
     return newArr
 }
 print(selectionSort(arr: [5, 3, 2, 6, 10]))
+
+func countDown(i: Int) {
+    print(i)
+    if i <= 0 {
+        return
+    } else {
+        countDown(i: i - 1)
+    }
+}
+
+countDown(i: 4)
+
+func fact(x: Int) -> Int {
+    if x <= 1 { return 1 }
+    else {
+        return x * fact(x: x - 1)
+    }
+}
+
+fact(x: 5)
+
+func sumRecursive(arr: [Int]) -> Int {
+    if arr.count == 0 {
+        return 0
+    } else if arr.count == 1 {
+        return arr[0]
+    } else {
+        return arr[0] + sumRecursive(arr: Array(arr.dropFirst()))
+    }
+}
+
+sumRecursive(arr: [2, 4, 6])
